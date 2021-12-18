42f26d6b71b04ef62c163f99125da840fb91a8da
yakintech <cagatayyildiz87@gmail.com>
2021-12-18 13:28:41 +0300

    project init

--- SocketSample/ContentView.swift
+++ SocketSample/ContentView.swift
@@ -6,11 +6,62 @@
 //
 
 import SwiftUI
+import SocketIO
+
+
+
+
+
 
 struct ContentView: View {
+    
+    @State var messages = [String]()
+    @State var sendmessage = ""
+    let manager = SocketManager(socketURL: URL(string: "http://localhost:3000")!, config: [.log(false), .compress])
+
+   
+    
     var body: some View {
-        Text("Hello, world!")
+        VStack{
+          
+            
+            TextField("your message: ", text: $sendmessage)
+                .padding()
+            
+            Button("Send"){
+                
+                let socket = manager.defaultSocket
+            
+      
+                socket.emit("sendermessage", sendmessage)
+            }
             .padding()
+            
+         
+            List(messages, id:\.self){item in
+                Text(item)
+            }
+            
+        }
+        .onAppear(){
+            let socket = manager.defaultSocket
+            
+            
+            socket.on("newmessage") {data, ack in
+              
+                guard let newMessage = data[0] as? String else { return }
+                
+                messages.append(newMessage)
+                
+                print(messages)
+                
+            }
+
+
+            socket.connect()
+            
+        }
+     
     }
 }
 
