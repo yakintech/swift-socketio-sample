094268ac7d3bd10a8a8c68a12d8005aba8091e06
yakintech <cagatayyildiz87@gmail.com>
2021-12-18 12:07:49 +0300

    Initial Commit

---
+++ SocketSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SocketSample
+//
+//  Created by Çağatay Yıldız on 18.12.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SocketSample/SocketSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocketSampleApp.swift
+//  SocketSample
+//
+//  Created by Çağatay Yıldız on 18.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct SocketSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
